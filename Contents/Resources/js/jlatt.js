ffc8c2bca7485093350073938d0e4830230b7e2e
Jeremy Latt <jeremy.latt@gmail.com>
2013-05-09 12:21:50 -0700

    Add some cleanup support for hipchat.

---
+++ Contents/Resources/js/jlatt.js
@@ -0,0 +1,15 @@
+$('#Chat').on('DOMNodeInserted', '.line', function(e) {
+    var $line = $(this);
+    if ($line.is('.hipchat-fixed')) {
+        return;
+    }
+
+    $line.addClass('hipchat-fixed');
+    var sender = $line.find('.sender .name').text();
+    if (!(/^(?:github|jenkins)$/i).test(sender)) {
+        return;
+    }
+
+    var $text = $line.find('.text');
+    $text.html($text.text());
+});
