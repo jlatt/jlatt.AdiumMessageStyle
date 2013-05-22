9b51015db5d56de7445f06e08d03ec012565ee87
Jeremy Latt <jeremy.latt@gmail.com>
2013-05-21 18:22:35 -0700

    Add IRC notice detection.

--- Contents/Resources/js/jlatt.js
+++ Contents/Resources/js/jlatt.js
@@ -1,9 +1,20 @@
-$('#Chat').on('DOMNodeInserted', '.line:not(.html-fixed)', function(e) {
-    var $line = $(this).addClass('html-fixed');
-    var sender = $line.find('.sender .name').text();
-    if (!(/^(GitHub|Jenkins|Heroku|Pivotal)$/).test(sender)) {
-        return;
-    }
-    var $text = $line.find('.text');
-    $text.html($text.text());
-});
+(function() {
+    var NOTICE_REGEX = /^\(notice\) '/;
+    var HTML_SENDER_REGEX = /^(GitHub|Jenkins|Heroku|Pivotal)$/;
+
+    $('#Chat').on('DOMNodeInserted', '.line:not(.html-fixed)', function(e) {
+        var $line = $(this).addClass('html-fixed');
+        var $text = $line.find('.text');
+        var message = $text.text();
+
+        if (NOTICE_REGEX.test(message)) {
+            message = message.replace(NOTICE_REGEX, '');
+            $line.addClass('notice');
+        }
+
+        var sender = $line.find('.sender .name').text();
+        if (HTML_SENDER_REGEX.test(sender)) {
+            $text.html(message);
+        }
+    });
+}).call(this);
