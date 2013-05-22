f6a8c358a92ef2efea44846366518cc9292a9152
Jeremy Latt <jeremy.latt@gmail.com>
2013-05-22 13:23:19 -0700

    Fix notice detection.

--- Contents/Resources/js/jlatt.js
+++ Contents/Resources/js/jlatt.js
@@ -1,20 +1,20 @@
 (function() {
-    var NOTICE_REGEX = /^\(notice\) '/;
+    var NOTICE_REGEX = /^\(notice\)\s*/;
     var HTML_SENDER_REGEX = /^(GitHub|Jenkins|Heroku|Pivotal)$/;
 
     $('#Chat').on('DOMNodeInserted', '.line:not(.html-fixed)', function(e) {
         var $line = $(this).addClass('html-fixed');
         var $text = $line.find('.text');
-        var message = $text.text();
 
+        var message = $text.html();
         if (NOTICE_REGEX.test(message)) {
-            message = message.replace(NOTICE_REGEX, '');
+            $text.html(message.replace(NOTICE_REGEX, ''));
             $line.addClass('notice');
         }
 
         var sender = $line.find('.sender .name').text();
         if (HTML_SENDER_REGEX.test(sender)) {
-            $text.html(message);
+            $text.html($text.text());
         }
     });
 }).call(this);
